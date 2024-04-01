679247112e7a230e0510ee978c88c19d7ccdfe90
Patrick Woods <patrick.woods@usda.gov>
2024-04-01 16:23:54 -0600

    Updating the README file and fixing a grammar issue in the R script.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Hop_release
+# README file for Hop_release project. #
+# Author: Patrick Woods #
+# Date: April 1, 2024 #
+
 Code used to analyze powdery mildew data and plot the results for the hop release project.
+
+This repo contains two scripts: 1) Reaction_norm_plotting_script.R and 2) python_script_for_calculation_mean_colony_counts.py
+
+The first script is used to plot the reaction norm style figure used in the publication.
+
+The second script is used to obtain both the summary mean colony counts for each hop line included in the study as well as prepare the data for plotting the reaction norm.
--- Reaction_norm_plotting_script.R
+++ Reaction_norm_plotting_script.R
@@ -1,18 +1,23 @@
 ### Script for Plotting a Reaction Norm Using Powdery Mildew Screen Data for Hop Release ###
 ## Author: Patrick Woods ##
-## Date: March 27th, 2024 ##
+## Date: April 1, 2024 ##
 
 
-
+library(ggplot2)
 pm <- read.csv(file.choose(),header=T)
 
 pm$Identifier <- as.factor(pm$Identifier)
 pm$Dish <- as.factor(pm$Dish)
 pm$Mildew <- as.factor(pm$Mildew)
+pm$Mildew_Code <- as.factor(pm$Mildew_Code)
 
-
-ggplot(pm, aes(x = Mildew, y = Colony.count)) +
+#reaction norm plot#
+ggplot(pm, aes(x = Mildew_Code, y = Colony.count)) +
        stat_summary(aes(group = Identifier, color = Identifier), fun.y = mean, geom = "line") +
        stat_summary(aes(color = Identifier), fun.data = mean_se, geom = "errorbar", width = 0.1) +
        stat_summary(aes(color = Identifier), fun.y = mean, geom = "point", size = 4, alpha=0.6) +
-       xlab('Powdery Milew Race') + ylab('Mean Colony Counts')  + theme_bw()
+       xlab('Powdery Mildew Race') + ylab('Mean Colony Counts')  + theme_bw()
+
+
+
+
